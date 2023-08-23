d02f9fc073c257117655b8a8f2c85370ad860e4f
Anouar El maaroufi <ael-maar@c1r8p6.1337.ma>
2023-08-23 13:14:31 +0100

    Fixed the checks for rgb color
    Not encountering the commas when splitting the rgb string component

--- map_parser/valid_map_scene_cmps.c
+++ map_parser/valid_map_scene_cmps.c
@@ -6,11 +6,11 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 12:48:32 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/08/19 15:43:34 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/08/23 12:26:53 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub.h"
+#include "../cub.h"
 
 bool	are_textures_valid(char *components[], t_map_info *map_scene)
 {
@@ -26,22 +26,25 @@ bool	are_textures_valid(char *components[], t_map_info *map_scene)
 		close(fd);
 		i++;
 	}
-	map_scene->no_text	= components[NO];
-	map_scene->so_text	= components[SO];
-	map_scene->we_text	= components[WE];
-	map_scene->ea_text	= components[EA];
+	map_scene->no_text = components[NO_TEXT];
+	map_scene->so_text = components[SO_TEXT];
+	map_scene->we_text = components[WE_TEXT];
+	map_scene->ea_text = components[EA_TEXT];
 	return (true);
 }
 
 bool	is_color_unit_valid(char *color_unit)
 {
+	int	i;
+
 	if (ft_strlen(color_unit) > 3)
 		return (false);
-	while (*color_unit)
+	i = 0;
+	while (color_unit[i])
 	{
-		if (!ft_isdigit(*color_unit))
+		if (!ft_isdigit(color_unit[i]))
 			return (false);
-		color_unit++;
+		i++;
 	}
 	if (ft_atoi(color_unit) > 255)
 		return (false);
@@ -53,17 +56,7 @@ u_int32_t	rgb_units_to_dec(int r, int g, int b)
 	return ((r << 24) | (g << 16) | (b << 8) | 0XFF);
 }
 
-int	len_2d_arr(char **arr)
-{
-	int	arr_len;
-
-	arr_len = 0;
-	while (arr[arr_len])
-		arr_len++;
-	return (arr_len);
-}
-
-bool	is_valid_color(char *color_cmp, u_int32_t *color)
+bool	is_valid_rgb_color(char *color_cmp, u_int32_t *color)
 {
 	char	**rgb;
 	int		i;
@@ -71,7 +64,7 @@ bool	is_valid_color(char *color_cmp, u_int32_t *color)
 	rgb = ft_split(color_cmp, ',');
 	if (!rgb)
 		exit(EXIT_FAILURE);
-	if (len_2d_arr(rgb) != 3)
+	if (map_len(rgb) != comma_len(color_cmp) + 1)
 	{
 		free_2d_arr(rgb);
 		return (false);
@@ -93,11 +86,11 @@ bool	is_valid_color(char *color_cmp, u_int32_t *color)
 
 void	valid_map_scene_cmps(char *components[], t_map_info *map_scene)
 {
-	if (!are_textures_valid(components))
+	if (!are_textures_valid(components, map_scene))
 		error_map_cmps("Invalid Texture Path", NULL, components, map_scene);
-	if (!is_valid_color(components[4], &(map_scene->floor_clr)))
+	if (!is_valid_rgb_color(components[4], &(map_scene->floor_clr)))
 		error_map_cmps("Invalid RGB Color", NULL, components, map_scene);
-	if (!is_valid_color(components[5], &(map_scene->ceil_clr)))
+	if (!is_valid_rgb_color(components[5], &(map_scene->ceil_clr)))
 		error_map_cmps("Invalid RGB Color", NULL, components, map_scene);
 	free(components[4]);
 	free(components[5]);
