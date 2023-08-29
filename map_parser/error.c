f037f91d2ca5e05dec2a443b22355d43f4f32b2d
Anouar El maaroufi <ael-maar@student.1337.ma>
2023-08-29 20:25:37 +0100

    Changing the free_map_scene from error.c to free.c

--- map_parser/error.c
+++ map_parser/error.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 12:05:37 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/08/29 18:46:51 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/08/29 20:24:34 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,4 @@ void	error_map_cmps(char *message, char **component, \
 		free(component_checks[i++]);
 	free(map_scene);
 	error_message(message);
-}
-
-void	free_map_scene(t_map_info *map_scene)
-{
-	free_2d_arr(map_scene->grid);
-	free(map_scene->no_text);
-	free(map_scene->so_text);
-	free(map_scene->we_text);
-	free(map_scene->ea_text);
-	free(map_scene);
 }
