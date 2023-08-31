0ae3ed000383143a39d70b5df7ad2c2787d2a81d
Yahya Abad <yabad@c1r5p10.1337.ma>
2023-08-31 15:05:16 +0100

    letting the free map scene handle the textures deallocation from memory

--- free.c
+++ free.c
@@ -6,30 +6,15 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 20:26:23 by yabad             #+#    #+#             */
-/*   Updated: 2023/08/30 17:03:33 by yabad            ###   ########.fr       */
+/*   Updated: 2023/08/30 20:59:35 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-void	free_map_scene(t_map_info *map)
-{
-	(void)map;
-	return ;
-}
-
-void	free_textures(t_mlx *mlx)
-{
-	free(mlx->map->ea_text);
-	free(mlx->map->no_text);
-	free(mlx->map->so_text);
-	free(mlx->map->we_text);
-}
-
 void	safe_exit(t_mlx *mlx)
 {
 	free_map_scene(mlx->map);
-	free_textures(mlx);
 	if (mlx->img)
 		mlx_delete_image(mlx->mlx, mlx->img);
 	if (mlx->img_3d)
